119ba6a1c6918a7a6850edc4532b37e600155ebd
hammadirshad19 <hammad.irshad@xloopdigital.com>
2024-01-30 13:50:02 +0500

    delete previous data

--- Backend/brandguard_app_v3/brandguard_app/app/extensions.py
+++
@@ -1,7 +0,0 @@
-from apscheduler.schedulers.background import BackgroundScheduler
-from flask_migrate import Migrate
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
-migrate = Migrate()
-scheduler = BackgroundScheduler()
--- Backend/brandguard_app_v3/brandguard_app/start_app.sh
+++
@@ -1,8 +0,0 @@
-# start_app.sh
-
-export SECRET_KEY="12345"
-export DATABASE_URI="postgresql://postgres:12345@localhost:5432/brandguard"
-export FLASK_APP=app.factory:create_app
-export FLASK_ENV=development
-
-flask run
