596fa60fc30c850fde6481a338a61028e65b8ee9
Muhammad-Danish-011 <muhammad.danish@xloopdigital.com>
2024-02-06 13:11:28 +0500

    Backend Frontend added

---
+++ Frontend/src/menu-items/index.js
@@ -0,0 +1,14 @@
+// project import
+import dashboard from './dashboard';
+import Report from './report';
+
+// ==============================|| MENU ITEMS ||============================== //
+
+const menuItems = {
+  items: [ dashboard,Report]
+  // if you want to add login and signup so uncomment below line and remove above
+  // items: [ pages,dashboard,Report]
+
+};
+
+export default menuItems;
