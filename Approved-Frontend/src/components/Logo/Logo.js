b8e5e025a13fd976c868e1b74b408873197d67f6
Muhammad Danish <127726082+Muhammad-Danish-011@users.noreply.github.com>
2024-02-06 10:16:27 +0500

    Delete Approved-Frontend directory

--- Approved-Frontend/src/components/Logo/Logo.js
+++
@@ -1,9 +0,0 @@
-import logo from '../../assets/images/brandguard_logo_black.png';
-
-// ==============================|| LOGO SVG ||============================== //
-
-const Logo = () => {
-  return <img src={logo} alt="Brandguard" width="180" />;
-};
-
-export default Logo;
